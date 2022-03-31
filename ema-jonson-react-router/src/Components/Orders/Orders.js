54c8cdf63b293259cd79081af86757db4e2d1823
anamerasul <anamerasul@gmail.com>
2022-03-31 19:25:54 +0600

    Create useCart with products dependency and use it

--- ema-jonson-react-router/src/Components/Orders/Orders.js
+++ ema-jonson-react-router/src/Components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import UseCart from '../Hooks/UseCart';
 import UseProducts from '../Hooks/UseProduct';
 import './Orders.css'
 const Orders = () => {
     const [product, setProducts] = UseProducts()
+    const [cart, setCart] = UseCart(product)
     return (
         <div>
             <h2 className="bg-primary"> this is Orders {product.length}</h2>
+            <p>cart has {cart.length}</p>
         </div>
     );
 };
